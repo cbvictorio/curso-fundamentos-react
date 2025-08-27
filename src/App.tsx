e1be66cbe130d99178da14df89254b53d99b8677
Cesar Victorio <cbvictorio@gmail.com>
2025-08-26 19:30:57 -0700

    add FormWithState logic and implementation for the 2nd approach example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import FormData from './components/FormData'
+import FormWithState from './components/FormWithState'
 
 function App() {
   return (
     <div className='w-1/2 mx-auto p-3'>
       <h1 className='font-extrabold text-center my-5'> Curso React </h1>
-      <FormData />
+      <FormWithState />
+      {/* <FormData /> */}
     </div>
   )
 }
