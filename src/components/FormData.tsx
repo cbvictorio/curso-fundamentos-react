affa74acf8eec245036485a8e5a1c7b230a8b9c6
Cesar Victorio <cbvictorio@gmail.com>
2025-08-26 19:19:34 -0700

    implemented FormData first approach logic

--- src/components/FormData.tsx
+++ src/components/FormData.tsx
@@ -1,14 +1,22 @@
-import { type FormEvent } from 'react'
+import { useRef, type FormEvent } from 'react'
 
 function FormDataComponent() {
 
+    const formRef = useRef(null)
+
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        if (formRef.current) {
+            const formData = new FormData(formRef.current)
+            const entries = formData.entries()
+            const formFields = Object.fromEntries(entries)
+            console.log(formFields)
+        }
     };
 
 
     return (
-        <form onSubmit={handleSubmit} className="space-y-6 max-w-md mx-auto p-[50px] text-white">
+        <form ref={formRef} onSubmit={handleSubmit} className="space-y-6 max-w-md mx-auto p-[50px] text-white">
             <input
                 type="text"
                 placeholder="Nombre"
@@ -27,38 +35,6 @@ function FormDataComponent() {
                 name="telephone"
                 className="w-full px-4 py-3 border-2 border-purple-300 rounded-lg focus:outline-none focus:border-purple-500 focus:ring-2 focus:ring-purple-200 transition-all duration-300 placeholder-gray-500 shadow-sm hover:shadow-md"
             />
-             <div className="flex items-center">
-                <input
-                    type="checkbox"
-                    id="red"
-                    name="red"
-                    value="red"
-                    className="w-4 h-4 text-red-600 bg-gray-100 border-gray-300 rounded focus:ring-red-500"
-                />
-                <label
-                    htmlFor="red"
-                    className="ml-2 text-sm font-medium flex items-center"
-                >
-                    <span className="w-4 h-4 bg-red-500 rounded-full mr-2"></span>
-                    Rojo
-                </label>
-            </div>
-            <div className="flex items-center">
-                <input
-                    type="checkbox"
-                    id="blue"
-                    name="blue"
-                    value="super-azul"
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
-                />
-                <label
-                    htmlFor="blue"
-                    className="ml-2 text-sm font-medium flex items-center"
-                >
-                    <span className="w-4 h-4 bg-blue-500 rounded-full mr-2"></span>
-                    Azul
-                </label>
-            </div>
             <button
                 type="submit"
                 className="w-full bg-gradient-to-r from-blue-500 to-purple-600 text-white py-3 px-6 rounded-lg font-semibold hover:from-blue-600 hover:to-purple-700 transform hover:scale-105 transition-all duration-300 shadow-lg hover:shadow-xl"
